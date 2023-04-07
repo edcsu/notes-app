65422b958911cc021605ca7ef9c3a8953a484449
Ssewannonda Keith Edwin <skeith696@gmail.com>
2023-04-07 18:25:10 +0300

    feat: add navbar

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,52 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
-    <TheWelcome />
+    <div class="container">
+      <header>
+        <h1 id="app-text">Yo Notes</h1>
+        <button id="add-btn">+</button>
+      </header>
+      <div class="cards-container">
+        <div class="card">
+          <p class="card-text"></p>
+          <p class="muted-text"></p>
+        </div>
+      </div>
+    </div>
   </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
+  main {
+    height: 100vh;
+    width: 100vw;
+  }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+  .container {
+    max-width: 1000px;
+    padding: 0.5rem;
+    margin: 0 auto;
+  }
 
-@media (min-width: 1024px) {
   header {
     display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+    justify-content: space-between;
+    align-items: center;
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
+  #app-text {
+    font-weight: bold;
+    margin-bottom: 0.5rem;
+    font-size: 4rem;
   }
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+  #add-btn {
+    border: none;
+    border-radius: 100%;
+    width: 2rem;
+    height: 2rem;
+    cursor: pointer;
+    color: white;
+    background-color: rgb(21, 20, 20);
+    font-size: 1rem;
   }
-}
 </style>
